dd68b5001788fa94fb3d769773188ba36c5edb7d
milly1110ning-dot <milly.1110ning@gmail.com>
2026-03-20 11:29:52 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Test
+{
+	public int Mid;
+	public int Final;
+
+	public void SetFinal(int value)
+	{
+		Final = value;
+	}
+
+	public int GetFinal()
+	{
+        return Final;
+    }
+
+	public double GetAvg()
+	{
+		return (Mid + Final)/2.0;
+	}
+
+	public void SetGrate(int m,int f)
+	{
+		if (ValidGrade(m, f))
+		{
+			Mid = m;
+			Final = f;
+		}
+	} 
+
+	private bool ValidGrade(int m,int f)
+	{
+		if (m >= 0 && m <= 100)
+		{
+			if (f >= 0 && f <= 100)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Class_0320_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1, std2;
+
+            std1 = new Test();
+            std2 = new Test();
+
+            std1.Mid = 78;
+            std1.SetFinal(72);
+
+            std2.SetGrate(68,84);
+
+
+            Console.WriteLine("第一位" + std1.GetAvg().ToString());
+
+            Console.WriteLine("第二位" + std2.GetAvg().ToString());
+
+
+
+        }
+    }
+}
